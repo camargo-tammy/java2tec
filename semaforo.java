d58a83284f01e2e786d664fc42d16cd6e6b1e6ad
Tamirys Hariadny Camargo Couto <tamirys.couto@escola.pr.gov.br>
2026-06-22 17:21:07 +0000

    11

--- semaforo.java
+++ semaforo.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class semaforo {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
+Scanner scanner = new Scanner(System.in);
+      
 
         System.out.println("Digite uma cor (Verde, Amarelo ou Vermelho): ");
         String cor = scanner.nextLine();
         System.out.println(cor);
-        if (cor == "Verde") {
+        if (cor.equals( "Verde")) {
             System.out.println("Siga");
-        } else if (cor == ("Amarelo")) {
+        } else if (cor.equals("Amarelo")) {
             System.out.println("Atenção");
-        } else if (cor == ("Vermelho")) {
+        } else if (cor.equals ("Vermelho")) {
             System.out.println("Pare");
         } else {
             System.out.println("Cor inválida");
